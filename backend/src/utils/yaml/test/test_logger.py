2819506f73acac6d4ddda552bd3e825e7b422920
Jeong Yo Han <ingyeoking13@gmail.com>
2023-11-20 01:15:38 +0900

    feature: settings ${{ }} 환경변수 지원
    - 환경변수를 그리디하게 참조합니다.

---
+++ backend/src/utils/yaml/test/test_logger.py
@@ -0,0 +1,32 @@
+import pytest
+import re
+from pathlib import Path
+from src.utils.yaml.yaml import replace_env_from_yaml
+import yaml
+
+def test_load_yaml(monkeypatch):
+    _yaml = """
+        test:
+            name: hello
+            env: ${{ exist_env }}:noworld
+            source: ${{ none_env }}
+
+        test2:
+            name: bye:flah
+            env: ${{ exist_env }}
+            source: ${{ none_env}}:noworld
+    """
+    monkeypatch.setenv('exist_env', 'hello world')
+    result = replace_env_from_yaml(yaml.safe_load(_yaml))
+    assert result == {
+        'test': {
+            'name': 'hello',
+            'env': 'hello world',
+            'source': None
+        },
+        'test2': {
+            'name': 'bye:flah',
+            'env': 'hello world',
+            'source': 'noworld'
+        }
+    }
