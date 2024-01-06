a350dc05259ca5ed67068f85f7492258d4879a92
Jeong Yo Han <ingyeoking13@gmail.com>
2024-01-06 15:04:49 +0900

    feature: sign-in, sign-out implementation

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -13,17 +13,43 @@ import {
   MenuItem,
   Button,
 } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Response, request } from '@@/common/fetch';
+import { useRouter } from 'next/navigation';
 
 export const AppBarComponent = () => {
+  const router = useRouter();
   const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
+  const [loginStatus, setLoginStatus] = useState<boolean>(false);
+
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElement(event.currentTarget);
   };
 
+  const handleLogout = async () => {
+    const { data } = (await request({
+      url: "/auth/signout",
+      method: "post",
+      body: ''
+    })) as Response;
+    if (data) {
+      window.localStorage.removeItem('access_token');
+      window.location.reload();
+    } else {
+      alert('logout error.')
+    }
+  }
+
+  useEffect(() => {
+    const accessToken = window.localStorage.getItem('access_token')
+    if(accessToken) {
+      setLoginStatus(true);
+    }
+  },[])
+
   const repoUrl = 'https://github.com/ingyeoking13';
   return (
-    <AppBar>
+    <AppBar component="nav">
       <Toolbar>
         <Link href={repoUrl} target="_blank">
           <IconButton>
@@ -38,11 +64,27 @@ export const AppBarComponent = () => {
             anchorEl={anchorElement}
             open={Boolean(anchorElement)}
             onClose={() => setAnchorElement(null)}
-            anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
           >
-            <MenuItem>회원가입</MenuItem>
-            <MenuItem>로그인</MenuItem>
-            <MenuItem>로그아웃</MenuItem>
+            <MenuItem>
+              <Button href="/signup" color="primary" LinkComponent={Link}>
+                회원가입
+              </Button>
+            </MenuItem>
+            {!loginStatus && (
+              <MenuItem>
+                <Button href="/signin" color="primary" LinkComponent={Link}>
+                  로그인
+                </Button>
+              </MenuItem>
+            )}
+            {loginStatus && (
+              <MenuItem>
+                <Button onClick={handleLogout} color="primary">
+                  로그아웃
+                </Button>
+              </MenuItem>
+            )}
           </Menu>
         </Box>
       </Toolbar>
--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,57 +1,115 @@
-import { request } from '@@/common/fetch';
+import { Response, request } from '@@/common/fetch';
+import { AppBarComponent } from '@@/components/AppBar';
 import { Label } from '@mui/icons-material';
-import { Box, Button, FormControl, Input, InputLabel } from '@mui/material';
+import { AppBar, Box, Button, FormControl, Input, InputLabel, Toolbar, Typography } from '@mui/material';
 import React, { useState } from 'react';
 
 interface SignUp {
-    id: string;
+    name: string;
     password: string;
 }
 
-const SignInPage = () => {
-  const [signup, setSignUp] = useState<SignUp>({id: '', password: ''});
+const SignUpPage = () => {
+  const [signup, setSignUp] = useState<SignUp>({name: '', password: ''});
+  const [successMessage, setSuccessMessage] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
-    const result = await request({
-      url: 'signup',
-      method: 'POST',
-      body: signup,
-    })
+    try {
+      const { data, error_message } = await request({
+        url: "auth/signup",
+        method: "POST",
+        body: signup,
+      }) as Response;
+      if (data) {
+        setSuccessMessage('회원가입에 성공하였습니다.')
+        setErrorMessage('')
+      }
+      else {
+        setSuccessMessage('');
+        setErrorMessage(error_message)
+      }
+    } catch (e) {
+      setErrorMessage(`${e}`);
+      setSuccessMessage('')
+    }
   };
 
   const update = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
     type: string
   ) => {
-    if (type == 'id') {
+    if (type == 'name') {
       setSignUp((prev) => ({
-        id: event.target.value,
+        name: event.target.value,
         password: prev.password,
       }));
     } else if (type == 'password') {
       setSignUp((prev) => ({
-        id: prev.id,
-        password: prev.password,
+        name: prev.name,
+        password: event.target.value,
       }));
     }
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit}>
-      <FormControl>
-        <InputLabel>아이디</InputLabel>
-        <Input id="sign-id" onChange={(event) => update(event, 'id')}></Input>
-      </FormControl>
-      <FormControl>
-        <InputLabel>패스워드</InputLabel>
-        <Input id="sign-pw" type="password"></Input>
-      </FormControl>
-      <Button type="submit" variant="contained" color="primary">
-        회원가입
-      </Button>
+    <Box>
+      <AppBarComponent />
+      <Toolbar />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          mt: 10,
+        }}
+      >
+        <Typography variant="h4">회원가입</Typography>
+        <Box
+          sx={{
+            border: 1,
+            borderColor: "#516dcb",
+            borderRadius: 0.5,
+            width: 300,
+            p: 5,
+          }}
+        >
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{ mt: 3, display: "flex", flexDirection: "column" }}
+          >
+            <FormControl>
+              <InputLabel>아이디</InputLabel>
+              <Input
+                id="sign-id"
+                onChange={(event) => update(event, "name")}
+              ></Input>
+            </FormControl>
+            <FormControl sx={{ mt: 5 }}>
+              <InputLabel>패스워드</InputLabel>
+              <Input
+                id="sign-pw"
+                type="password"
+                onChange={(event) => update(event, "password")}
+              />
+            </FormControl>
+            <Button
+              sx={{ mt: 5 }}
+              type="submit"
+              variant="contained"
+              color="primary"
+            >
+              회원가입
+            </Button>
+            <Typography color="primary">{successMessage}</Typography>
+            <Typography color="error">{errorMessage}</Typography>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
 
-export default SignInPage;
+export default SignUpPage;
