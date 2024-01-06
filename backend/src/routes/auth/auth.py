2aed90a45f1c8218314307da0ba20a6af5d56e39
Jeong Yo Han <ingyeoking13@gmail.com>
2024-01-06 15:03:55 +0900

    feature: sign out implementation and remove basic-response

--- backend/src/routes/auth/auth.py
+++ backend/src/routes/auth/auth.py
@@ -1,8 +1,8 @@
 from src.repository.user.repo import UserRepo
 from src.dao.user.user import User
 from src.models.user.auth import Auth
-from src.models.response.response import BasicResponse, Content
-from fastapi import APIRouter, Depends, Response
+from src.models.response.response import BasicResponse, Content, Response
+from fastapi import APIRouter, Depends, Cookie
 from datetime import datetime, timedelta
 import jwt
 
@@ -20,40 +20,52 @@ class AuthRouter:
             else:
                 raise Exception('사용자가 존재합니다.')
         except Exception as e:
-            return BasicResponse(
-                Content(data=False, 
+            return Content(data=False, 
                         error_code=100,
-                        error_message=e.args[0]))
-        return BasicResponse(Content(data=True))
+                        error_message=e.args[0])
+        return Content(data=True)
     
-    @router.post('/signin', response_model=Content[bool])
+    @router.post('/signin', response_model=Content[dict])
     async def signin(auth: Auth, 
                      db: UserRepo = Depends(UserRepo)):
         try:
             if not db.check_user_exist(auth.name):
                 raise Exception('사용자가 존재하지 않습니다.')
             if db.check_password(auth):
-                response = BasicResponse(Content(data=True))
+                # access_token as response. 
+                # and cookie refresh (samesite strict approach)
+
+                response = BasicResponse(
+                    Content(data={
+                    'access_token': jwt.encode({
+                        'expire': (
+                            datetime.utcnow()+timedelta(minutes=30)
+                        ).isoformat()
+                    },secret_key, 'HS256')
+                }))
+
                 response.set_cookie(
-                    'access_token', 
+                    'refresh_token', 
                     jwt.encode({ 
-                        'expire': (datetime.utcnow() + 
-                                   timedelta(minutes=expire_minutes)).isoformat()
-                    }, secret_key, 
-                        'HS256'), 
-                   httponly=True, 
-                   max_age=1800)
+                        'id': auth.name,
+                    }, secret_key, 'HS256'), 
+                    httponly=True,
+                    max_age=3600*24*14,
+                    samesite='strict')
             else:
                 raise Exception('비밀번호가 일치하지 않습니다.')
         except Exception as e:
-            return BasicResponse(
-                Content(data=False,
+            return Content(data=None,
                         error_code=101,
                         error_message=e.args[0]
                 )
-            )
         return response
     
+    @router.post('/signout', response_model=Content[bool])
+    async def logout(response: Response):
+        response.delete_cookie('refresh_token')
+        return Content(data=True)
+
     @router.get('/users')
     async def users(db: UserRepo = Depends(UserRepo)):
         with db.session as session:
